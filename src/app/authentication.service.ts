aa2683fab4a2cc477ade0478374d71b6166d2d5d
rácz csaba <89366169+mokaesszorakozas@users.noreply.github.com>
2021-12-16 14:22:38 +0100

    Update authentication.service.ts

--- src/app/authentication.service.ts
+++ src/app/authentication.service.ts
@@ -23,12 +23,7 @@ export class AuthenticationService {
 
   login(username: string, password: string) {
     this.post = {'name':username,'password':password};
-    return this.http.post<any>(`${environment.apiUrl}/api/login`, this.post).pipe(map(user => {
-      // store user details and jwt token in local storage to keep user logged in between page refreshes
-      localStorage.setItem('currentUser', JSON.stringify(user));
-      this.currentUserSubject.next(user);
-      return user;
-    }));
+    return this.http.post<any>(`${environment.apiUrl}/api/login`, this.post);
   }
 
   logout() {
